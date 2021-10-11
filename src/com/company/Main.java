f8d81faea22db16418b546a094caade870d0ff79
Nikolai Syuris <nik.syu34@gmail.com>
2021-10-11 18:52:21 +0300

    mod2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("fghjgukhikjil");
+        System.out.println("0000000000000");
     }
 }
